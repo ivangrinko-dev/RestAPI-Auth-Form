b09c144cb1bb6ebf08358767cc45f864b1de7481
ivangrinko-dev <grinko.work.1982@mail.ru>
2023-08-04 22:28:29 +0300

    clientserver

---
+++ client/app.js
@@ -0,0 +1,26 @@
+const btnReg = document.querySelector(".btn-reg");
+const usernameInpReg = document.querySelector(".username-inp-reg");
+const emailInpReg = document.querySelector(".E-mail-inp-reg");
+const phoneInpReg = document.querySelector(".Phone-inp-reg");
+const passwordInpReg = document.querySelector(".Password");
+const confirmPasswordInpReg = document.querySelector(
+  ".Confirm-Password-inp-reg"
+);
+btnReg.addEventListener("click", async function () {
+  const objectToSerwer = {
+    username: usernameInpReg.value,
+    email: emailInpReg.value,
+    phone: phoneInpReg.value,
+    pwd: passwordInpReg.value,
+  };
+  
+  const response = await fetch("http://localhost:3000/api/register", {
+    method: "POST",
+    body: JSON.stringify(objectToSerwer),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  console.log(response.status);
+  console.log(await response.json());
+});
